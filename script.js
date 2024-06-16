c93046e7485e6d12a09d282c1bd5cd84c356e752
stringercube <140909265+stringercube@users.noreply.github.com>
2024-06-16 12:05:40 +0200

    calculator with extras finsihed

--- script.js
+++ script.js
@@ -2,54 +2,59 @@
 
 function add(a, b) {
 	return a + b;
-};
+}
 
 function subtract (a, b) {
 	return a - b;
-};
+}
 
 function multiply(a, b) {
     return a * b;
-};
-
-function divide(a, b) {
-    if (b != 0) {
-        return a / b;
-    } else {
-        return "Divide by 0";
-    };
-};
-
-function clear(display) {
-    display.textContent = "";
 }
 
+function divide(a, b) {
+    if (b != 0) {  
+        return a / b;
+    } else {
+        return 'Divide by 0'; // div by zero exception catch
+    }
+}
+
+// function to be called for clean screen
+
+function clear(display) {
+    display.textContent = ""; 
+}
+
+// function that appends decimal places in numbers
+
 function point(display) {
-    if (Number(display.textContent.substr(-1)) >= 0) {
-        let operator = getOperator(display.textContent);
-        let [a, b] = getNumbers(display.textContent, operator);
-        if ((b === 0 && a % 1 === 0) || (b !== 0 && b % 1 === 0)) {
+    if (Number(display.textContent.substr(-1)) >= 0) {     // check if last character is a number
+        let operator = getOperator(display.textContent);   // get operator to be looked for
+        let [a, b] = getNumbers(display.textContent, operator);        // get a and b
+        if ((b === 0 && a % 1 === 0) || (b !== 0 && b % 1 === 0)) {    // check if number to be appended has already decimals
             display.textContent += ".";        
-        };
-    };
-};
+        }
+    }
+}
 
 // special operation functions
 
 function power(a, b) {
 	return a ** b;
-};
+}
 
 function factorial(n) {
     if (n === 0) return 1;
     
     let product = 1;
-    for (let i = n; i > 0; i--) {
-        product *= i;
+    while (n > 0) {
+      product *= n;
+      n--;
     }
     
     return product;
-};
+  }
 
 function fibonacci(n) {
     if (n < 0) {
@@ -60,24 +65,25 @@ function fibonacci(n) {
         return 1
     } 
     
-    let a = 0, b = 1, fibo = 0;
+    let a = 0, b = 1;
     for (let i = 2; i <= n; i++) {
-        fibo = a + b;
         a = b;
-        b = fibo;
+        b = a + b;
     };
     
-    return fibo;
+    return b;
 };
+
+// function backspace removes the last character in display
 
 function backspace(display) {
     display.textContent = display.textContent.substr(0, display.textContent.length - 1);
 }
 
-// function operate
+// function operate, used for every operation requiring two numbers and operator in display
+// takes a, b, operator and display content as arguments
 
 function operate(a, b, operator, display) {
-    
     let result = 0;
 
     if (operator === 'division') {
@@ -94,13 +100,18 @@ function operate(a, b, operator, display) {
     display.textContent = result;
 }
 
+// event listeners added to buttons and operators
+// keyboard event listeners also added, with the exception of finonacci, factorial and exp functions
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Select all buttons with the class 'number'
-    const buttons = document.querySelectorAll('.number');
-    const operators = document.querySelectorAll('.function');
+
+// Select all buttons with the classes number and function
+
+    const buttons = document.querySelectorAll('.number'); 
+    const operators = document.querySelectorAll('.function'); 
 
     // Iterate over the NodeLists of buttons and operators and add event listeners
+    
     buttons.forEach(button => {
         button.addEventListener('click', inputKey)
         });
@@ -108,13 +119,17 @@ document.addEventListener('DOMContentLoaded', () => {
     operators.forEach(operator => {
         operator.addEventListener('click', inputOperator)
         });
-
+    
+    // add keydown event listeners, assigning them to existing function based on key pressed passing it to 
+    // the right input function
+    
     document.addEventListener('keydown', (e) => {
         const key = e.key;
-
-        if (key === ' ') return; // Prevent space input
+        const codeKey = e.code;
         
-        if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(key) || key === '.' || key === 'c') {
+        if (codeKey === 'Space') e.preventDefault(); // Prevent space input messing display
+        
+        if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', 'c'].includes(key)) {
             inputKey({ key: key });
         }
         
@@ -123,6 +138,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         });
     });
+
+// function to associate input to associated function based on event
 
 function inputKey(e) {
     let keyValue = e.currentTarget ? e.currentTarget.textContent : e.key;
@@ -142,13 +159,16 @@ function inputKey(e) {
     }
 }
 
+// function to associate input to associated function based on event
+
 function inputOperator(e) { 
     let keyValue = e.currentTarget ? e.currentTarget.id : e.key;
     const screenContent = document.querySelector('.display');
 
     if (keyValue === 'Backspace') {
         backspace(screenContent);
-    } else if (['/', '*', '-', '+', '^'].includes(keyValue)) {
+    // based on pressed key inputs operator in display declared in unicode
+    } else if (['/', '*', '-', '+', '^'].includes(keyValue)) { 
         if (Math.abs(Number(screenContent.textContent)) >= 0) {
             if (keyValue === '/') {
                 screenContent.textContent += '\u00F7';
@@ -160,23 +180,26 @@ function inputOperator(e) {
                 screenContent.textContent += '\u002B';
             } else if (keyValue === '^') {
                 screenContent.textContent += '^';
-            };
+            }
         }
+    // enter event triggers getting display context for variable declaration and further passing to functions
     } else if (keyValue === 'Enter') {
         const operator = getOperator(screenContent.textContent);
         const [a, b] = getNumbers(screenContent.textContent, operator);
         if (operator && !isNaN(b)) {
             operate(a, b, operator, screenContent);
         }
+    // for fibo and factorial inputs, only a number must be displayed in screen, which must be a integer
     } else if (keyValue === 'fibo' || keyValue === 'factorial') {
         const [a] = getNumbers(screenContent.textContent);
         if (Number.isInteger(a)) {
             const result = keyValue === 'fibo' ? fibonacci(a) : factorial(a);
-            screenContent.textContent = String(result);
+            screenContent.textContent = result;
         }
     }
 } 
 
+// function designed to get numbers whether there's a specific operator on screen or not
 function getNumbers(displayText, operator = "") {
     let separator = displayText.length;
     
@@ -197,6 +220,7 @@ function getNumbers(displayText, operator = "") {
     return [a, b];
 }
 
+// function that gets what operator is on display
 function getOperator(display) {
     if (display.includes('\u00F7')) {
         return 'division';
